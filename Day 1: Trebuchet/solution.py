ff7e3761e12aa23ff8b03419737497259fd6486f
Sanjay Nainwal <37777780+nainwalsanju@users.noreply.github.com>
2024-02-20 17:50:52 +0530

    updated solution

--- Day 1: Trebuchet/solution.py
+++ Day 1: Trebuchet/solution.py
@@ -1,27 +1,33 @@
 def process_file(file_path):
-    try:
-        with open(file_path, "r") as file:
-            for line in file:
-                print(line.strip())  # strip() removes leading and trailing whitespace, including the newline character
-    except FileNotFoundError:
-        print(f"File '{file_path}' not found.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+  try:
+    sum = 0
+    with open(file_path, "r") as file:
+      for line in file:
+        # print(line.strip())
+        sum += int(create_two_digit_number(line.strip()))
+        # print(sum)
+    print(sum)
+  except FileNotFoundError:
+    print(f"File '{file_path}' not found.")
+  except Exception as e:
+    print(f"An error occurred: {e}")
 
 
-def create_two_digit_number(input_string):
-    first_digit = ""
-    last_digit = ""
-    for char in input_string:
-        if char.isdigit():
-            if first_digit == "":
-                first_digit = char
-            last_digit = char
+def create_two_digit_number(input_string) -> str:
+  first_digit = ""
+  last_digit = ""
+  for char in input_string:
+    if char.isdigit():
+      if first_digit == "":
+        first_digit = char
+      last_digit = char
+  return first_digit + last_digit
 
 
 def main():
-    file_path = "input.txt"
-    process_file(file_path)
+  file_path = "input.txt"
+  process_file(file_path)
+
 
 if __name__ == "__main__":
-    main()
+  main()
